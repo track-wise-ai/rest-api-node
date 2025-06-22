c59714b573ace6ffd0ca779daa8a3ff8290c2b3f
ilia makarov <ilia.makarov@me.com>
2025-06-22 15:35:41 +0300

    made `/ping` as a public route

--- src/root/root.controller.ts
+++ src/root/root.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Auth } from '../iam/authentication/decorators/auth.decorator';
+import { AuthType } from '../iam/authentication/enums/auth-type.enum';
 
 @Controller('')
 export class RootController {
+  @Auth(AuthType.None)
   @Get('/ping')
   ping() {
     return 'pong';
